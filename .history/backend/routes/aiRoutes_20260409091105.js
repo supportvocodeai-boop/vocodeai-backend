2ac05e7d98b15f24ea81ad5fdcf00884bac78e3f
Saksham Agarwal <sakshamagarwal706@gmail.com>
2026-05-16 12:13:09 +0530

    Clean backend structure

--- .history/backend/routes/aiRoutes_20260409091105.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { auth } from "../middleware/auth.js";
-import { handleAI } from "../controllers/aiController.js";
-
-const router = express.Router();
-
-router.post("/", auth, handleAI);
-
-export default router;
--- backend/routes/statsRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getStats } from "../controllers/statsController.js";
-
-const router = express.Router();
-
-router.get("/", getStats);
-
-export default router;
